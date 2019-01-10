f4921072dbceba658fc10ba0edc866efba014423
YW-Tseng <ywwwtseng@gmail.com>
2019-01-10 17:03:11 +0800

    fix authLocalStoragePlugin bug

--- generator/templates/vuex-api-request/src/vendor/vuex-api-request.js
+++ generator/templates/vuex-api-request/src/vendor/vuex-api-request.js
@@ -1,21 +1,33 @@
+import store from '@/store'
 import VuexApiRequest, {
   Http,
   Auth, 
   createWatch, 
-  vuexApiModule, 
-  authLocalStoragePlugin,
+  vuexApiModule,
+  AuthLocalStoragePlugin,
 } from 'vuex-api-request'
 
 const vuexApiModuleName = 'api'
 const axiosConfig = {}
 
 const http = Http(axiosConfig)
-const auth = Auth(axiosConfig)
+const auth = Auth({
+  axiosConfig,
+  store,
+  authBinding: (state) => state.auth.accessToken,
+  authCompleted: (store) => store
+})
 
 const watch = createWatch({
   vuexApiModuleName,
-  response = (res) => res,
-  error = (err) => err,
+  response: (res) => res,
+  error: (err) => err,
+})
+
+const authLocalStoragePlugin = AuthLocalStoragePlugin({
+  storageKey: 'paradise-soft',
+  authModule: require('@/store/modules/auth'),
+  removeLocalStorageMutationType: 'clear',
 })
 
 export {
