1919a5629a7021797733fc4bb289e563ecaf284a
YW-Tseng <ywwwtseng@gmail.com>
2019-01-13 19:57:00 +0800

    use vuex-api-request@2.3.0

--- generator/vuex-api-request/template/src/vendor/vuex-api-request/authLocalStoragePlugin.js
+++ generator/vuex-api-request/template/src/vendor/vuex-api-request/authLocalStoragePlugin.js
@@ -3,7 +3,7 @@ import {LocalStoragePlugin} from 'vuex-api-request'
 const authLocalStoragePlugin = LocalStoragePlugin({
   storageKey: 'paradise-soft',
   vuexModule: ['auth', require('@/store/modules/auth').default],
-  clearWhen: (action) => action.type === 'clear'
+  clearWhen: (state, oldState) => !state.accessToken && oldState.accessToken,
 })
 
 export default authLocalStoragePlugin
