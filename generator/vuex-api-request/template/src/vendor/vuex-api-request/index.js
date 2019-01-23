c1a14c599b726238c3d23df414a063a95289f79d
ywwwtseng <ywwwtseng@gmail.com>
2019-01-23 19:57:55 +0800

    fix vue-api-request bug

--- generator/vuex-api-request/template/src/vendor/vuex-api-request/index.js
+++ generator/vuex-api-request/template/src/vendor/vuex-api-request/index.js
@@ -3,7 +3,7 @@ import VuexApiRequest from 'vuex-api-request';
 const watch = VuexApiRequest.createWatch({
   vuexApiModuleName: 'api',
   response: (res) => res,
-  error: (err) => err && err.response && err.response.data,,
+  error: (err) => err && err.response && err.response.data,
   errorHandler: (context, err) => {
     if (err.status === 401) context.dispatch('auth/logout');
   },
