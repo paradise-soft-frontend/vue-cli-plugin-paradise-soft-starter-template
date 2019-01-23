0a820383bcb4d6e7377d1bfb0bb5c868c10b2da3
ywwwtseng <ywwwtseng@gmail.com>
2019-01-23 09:43:33 +0800

    modified authLocalStoragePlugin

--- generator/vuex-api-request/template/src/vendor/vuex-api-request/authLocalStoragePlugin.js
+++ generator/vuex-api-request/template/src/vendor/vuex-api-request/authLocalStoragePlugin.js
@@ -4,7 +4,6 @@ import auth from '@/store/modules/auth';
 const authLocalStoragePlugin = LocalStoragePlugin({
   storageKey: 'paradise-soft',
   vuexModule: ['auth', auth],
-  clearWhen: (state, oldState) => !state.accessToken && oldState.accessToken,
 });
 
 export default authLocalStoragePlugin;
