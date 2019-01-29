8e3e2716079f29ec094d36b6afab1d5b9169d7a8
ywwwtseng <ywwwtseng@gmail.com>
2019-01-29 22:47:31 +0800

    auto register components folder's vue components

--- generator/default/template/src/components/ui/index.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-
-const requireModules = require.context('./', false, /\.vue/);
-requireModules.keys().forEach((filename) => {
-  const registerName = filename.replace(/(\.\/|\.vue)/g, '');
-  Vue.component(registerName, requireModules(filename).default);
-});
--- generator/default/template/src/main.js
+++ generator/default/template/src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import './components/ui';
 import '@/styles/main.scss';
+
+const componentModules = require.context('@/components', true, /\.vue/);
+componentModules.keys().forEach((filename) => {
+  let registerName = filename.replace(/(\.\/|\.vue)/g, '');
+  const startIndex = filename.replace(/(\.\/|\.vue)/g, '').lastIndexOf('/') + 1;
+  registerName = registerName.slice(startIndex);
+
+  Vue.component(registerName, componentModules(filename).default);
+});
 
 Vue.config.productionTip = false;
 
