2617507fc134ddc0fd146d91ff14a87f01ed0de1
ywwwtseng <ywwwtseng@gmail.com>
2019-03-20 09:41:56 +0800

    use vuex-api-request@2.13.1

--- generator/index.js
+++ generator/index.js
@@ -40,7 +40,7 @@ module.exports = (api, opts) => {
   if (opts.vuexApiRequest) {
     api.extendPackage({
       dependencies: {
-        'vuex-api-request': '^2.11.0',
+        'vuex-api-request': '^2.13.1',
       },
     });
 
